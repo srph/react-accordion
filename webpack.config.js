02756c71608260516d07638ed9507cd5bca94b74
Kier Borromeo <seraphipod@gmail.com>
2017-12-18 20:53:01 +0800

    Fix bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,42 @@
+var webpack = require('webpack');
+var path = require('path')
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'react-accordion': './src/index.js',
+    'react-accordion.min': './src/index.js'
+  },
+
+  externals: {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
 
   output: {
-    path: __dirname + '/dist',
-    filename: process.env.NODE_ENV === 'production'
-      ? 'react-accordion.min.js'
-      : 'react-accordion.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    libraryTarget: 'umd',
+    library: 'ReactAccordion',
   },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+      compress: { warnings: false }
+    })
+  ],
 
   module: {
     rules: [
